79547e9c52db11f3d17a54c81b9ebe99db436219
Dan Andrei <andreidani96@gmail.com>
2017-09-13 19:16:39 +0300

    Added image slider, image gallery.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,162 +1,136 @@
 /*
-	Overflow by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    Overflow by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var settings = {
+    var settings = {
+        // Full screen header?
+        fullScreenHeader: true,
+        // Parallax background effect?
+        parallax: true,
+        // Parallax factor (lower = more intense, higher = less intense).
+        parallaxFactor: 10
+    };
 
-		// Full screen header?
-			fullScreenHeader: true,
+    skel.breakpoints({
+        wide: '(max-width: 1680px)',
+        normal: '(max-width: 1080px)',
+        narrow: '(max-width: 840px)',
+        mobile: '(max-width: 736px)'
+    });
 
-		// Parallax background effect?
-			parallax: true,
+    $(function() {
+        var $window = $(window), $body = $('body');
 
-		// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 10
+        if (skel.vars.mobile) {
+            settings.parallax = false;
+            $body.addClass('is-scroll');
+        }
 
-	};
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-	skel.breakpoints({
-		wide: '(max-width: 1680px)',
-		normal: '(max-width: 1080px)',
-		narrow: '(max-width: 840px)',
-		mobile: '(max-width: 736px)'
-	});
+        $window.on('load', function() {
+            $body.removeClass('is-loading');
+        });
 
-	$(function() {
+        // CSS polyfills (IE<9).
+        if (skel.vars.IEVersion < 9) {
+            $(':last-child').addClass('last-child');
+        }
 
-		var	$window = $(window),
-			$body = $('body');
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-		if (skel.vars.mobile) {
+        // Prioritize "important" elements on mobile.
+        skel.on('+mobile -mobile', function() {
+            $.prioritize('.important\\28 mobile\\29', skel.breakpoint('mobile').active);
+        });
 
-			settings.parallax = false;
-			$body.addClass('is-scroll');
+        // Scrolly links.
+        $('.scrolly-middle').scrolly({
+            speed: 1000,
+            anchor: 'middle'
+        });
 
-		}
+        $('.scrolly').scrolly({
+            speed: 1000,
+            offset: function() { return (skel.breakpoint('mobile').active ? 70 : 190); }
+        });
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Full screen header.
+        if (settings.fullScreenHeader) {
+            var $header = $('#header');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+            if ($header.length > 0) {
+                var $header_header = $header.find('header');
 
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
+                $window.on('resize.overflow_fsh', function() {
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+                    if (skel.breakpoint('mobile').active) {
+                        $header.css('padding', '');
+                    } else {
+                        var p = Math.max(192, ($window.height() - $header_header.outerHeight()) / 2);
+                        $header.css('padding', p + 'px 0 ' + p + 'px 0');
+                    }
+                }).trigger('resize.overflow_fsh');
 
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
+                $window.load(function() {
+                    $window.trigger('resize.overflow_fsh');
+                });
+            }
+        }
 
-		// Scrolly links.
-			$('.scrolly-middle').scrolly({
-				speed: 1000,
-				anchor: 'middle'
-			});
+        // Parallax background.
+        // Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+        if (skel.vars.browser == 'ie' ||  skel.vars.mobile){
+            settings.parallax = false;
+        }
 
-			$('.scrolly').scrolly({
-				speed: 1000,
-				offset: function() { return (skel.breakpoint('mobile').active ? 70 : 190); }
-			});
+        if (settings.parallax) {
+            var $dummy = $(), $bg;
 
-		// Full screen header.
-			if (settings.fullScreenHeader) {
+            $window.on('scroll.overflow_parallax', function() {
+                // Adjust background position.
+                $bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+            }).on('resize.overflow_parallax', function() {
 
-				var $header = $('#header');
+                // If we're in a situation where we need to temporarily disable parallax, do so.
+                if (!skel.breakpoint('wide').active || skel.breakpoint('narrow').active) {
+                    $body.css('background-position', '');
+                    $bg = $dummy;
+                }
+                // Otherwise, continue as normal.
+                 else {
+                    $bg = $body;
+                }
 
-				if ($header.length > 0) {
+                // Trigger scroll handler.
+                $window.triggerHandler('scroll.overflow_parallax');
+            }).trigger('resize.overflow_parallax');
+        }
 
-					var $header_header = $header.find('header');
+        // Poptrox.
+        $('.image').poptrox({
+            useBodyOverflow: false,
+            usePopupEasyClose: false,
+            overlayColor: '#0a1919',
+            overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
+            usePopupDefaultStyling: false,
+            usePopupCaption: true,
+            popupLoaderText: '',
+            windowMargin: 10,
+            usePopupNav: true
+        });
 
-					$window
-						.on('resize.overflow_fsh', function() {
-
-							if (skel.breakpoint('mobile').active)
-								$header.css('padding', '');
-							else {
-
-								var p = Math.max(192, ($window.height() - $header_header.outerHeight()) / 2);
-								$header.css('padding', p + 'px 0 ' + p + 'px 0');
-
-							}
-
-						})
-						.trigger('resize.overflow_fsh');
-
-					$window.load(function() {
-						$window.trigger('resize.overflow_fsh');
-					});
-
-				}
-
-			}
-
-		// Parallax background.
-
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (skel.vars.browser == 'ie'
-				||	skel.vars.mobile)
-					settings.parallax = false;
-
-			if (settings.parallax) {
-
-				var $dummy = $(), $bg;
-
-				$window
-					.on('scroll.overflow_parallax', function() {
-
-						// Adjust background position.
-							$bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-
-					})
-					.on('resize.overflow_parallax', function() {
-
-						// If we're in a situation where we need to temporarily disable parallax, do so.
-							if (!skel.breakpoint('wide').active
-							||	skel.breakpoint('narrow').active) {
-
-								$body.css('background-position', '');
-								$bg = $dummy;
-
-							}
-
-						// Otherwise, continue as normal.
-							else
-								$bg = $body;
-
-						// Trigger scroll handler.
-							$window.triggerHandler('scroll.overflow_parallax');
-
-					})
-					.trigger('resize.overflow_parallax');
-
-			}
-
-		// Poptrox.
-			$('.gallery').poptrox({
-				useBodyOverflow: false,
-				usePopupEasyClose: false,
-				overlayColor: '#0a1919',
-				overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
-				usePopupDefaultStyling: false,
-				usePopupCaption: true,
-				popupLoaderText: '',
-				windowMargin: 10,
-				usePopupNav: true
-			});
-
-	});
+        // slick slider
+        $('.image').slick({
+            arrows: false,
+            dots: true
+        });
+    });
 
 })(jQuery);
